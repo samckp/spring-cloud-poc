3690878eda58382787676ce80608161793b2b24e
samckp <sam.mca.liet@gmail.com>
2019-05-07 21:43:08 +0530

    Values Read from properties file

--- withdraw-service/src/main/java/com/sam/withdrawservice/controller/WithdrawController.java
+++ withdraw-service/src/main/java/com/sam/withdrawservice/controller/WithdrawController.java
@@ -1,16 +1,21 @@
 package com.sam.withdrawservice.controller;
 
+import com.sam.withdrawservice.configuration.Configuration;
 import com.sam.withdrawservice.configuration.WithdrawConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WithdrawController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/withdraw")
     public WithdrawConfiguration withdrawConfiguration(){
 
-        return new WithdrawConfiguration(1000, 1);
+        return new WithdrawConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 
 }
